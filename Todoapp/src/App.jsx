c8b385151bf9cbab54fc537c28208da923ef20c8
sridevi <sridevim@dckap.com>
2026-09-11 17:30:50 +0530

    Created basic to do app

--- Todoapp/src/App.jsx
+++ Todoapp/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-
+  debugger;
+  const [task,setTask] = useState("");
+  const [list,setList] =useState([]);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setList([...list, task]);
+    setTask("");
+  }
   return (
     <>
-    <div className="text-2xl font-bold">
-      To Do App
+    <div className="text-lg font-bold text-blue-600">
+      To Do List
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Add a new task..." onChange={(e)=>setTask(e.target.value)}></input>
+        <button type="submit">Add Task</button>
+        <ul>
+          {list.map((listitem)=><li key="index">{listitem}</li>)}
+        </ul>
+      </form>
     </div>
-    
     </>
   )
 }
