95081401a65ca48061a485ec60bbb4fec236d11f
sridevi <sridevim@dckap.com>
2026-09-12 07:12:47 +0530

    Tried Edit,Done,Delete functionalities and rolled back to previous function becuase of some errors

--- Todoapp/src/App.jsx
+++ Todoapp/src/App.jsx
@@ -1,27 +1,40 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { MdEdit } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
+import { RxCross2 } from "react-icons/rx";
 import './App.css'
 
 function App() {
-  debugger;
   const [task,setTask] = useState("");
   const [list,setList] =useState([]);
+  const handleChange=(e)=>{
+    setTask(e.target.value);
+  }
   const handleSubmit=(e)=>{
+    debugger;
     e.preventDefault();
-    setList([...list, task]);
+    const trimmedTask = task.trim();
+    if( trimmedTask.length === 0 ) return;
+    setList((prevList) => [...prevList, trimmedTask]);
     setTask("");
+    console.log( list ,"list");
+  }
+  const handleEdit=(e)=>{
+    console.log( list , e.target.index);
+    setTask(list[e.target.index]);
   }
   return (
     <>
-    <div className="text-lg font-bold text-blue-600">
-      To Do List
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Add a new task..." onChange={(e)=>setTask(e.target.value)}></input>
-        <button type="submit">Add Task</button>
+      <div>
+        <p className="text-4xl font-bold text-blue-600">To Do List</p>
+        <form  >
+          <input type="text" placeholder="Add a new task..." value={task} className=" m-2 border text-xl font-semibold rounded-md py-4 px-2" onChange={handleChange}></input>
+          <button type="submit" className="border border-gray-200 bg-gray-600 hover-gray-200 text-xl font-semibold rounded-md py-4 px-2" onClick={handleSubmit}>Add Task</button>
+        </form>
         <ul>
-          {list.map((listitem)=><li key="index">{listitem}</li>)}
+          {list.map((listitem, index) => <li key={index} className="flex gap-2">{listitem}<MdEdit /><TiTick /><RxCross2  /></li>)}
         </ul>
-      </form>
-    </div>
+      </div>
     </>
   )
 }
