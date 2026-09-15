ae3752806fa012855fbab5c024098dd953afa362
sridevi <sridevim@dckap.com>
2026-09-16 04:32:48 +0530

    added api end point for editing,getting a list of todos,change the order of todos added,added varying id no

--- Todoapp/src/App.jsx
+++ Todoapp/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MdEdit } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
 import { RxCross2 } from "react-icons/rx";
@@ -9,7 +9,15 @@ function App() {
   const [task, setTask] = useState("");
   const [list, setList] = useState([]);
   const [editIndex, setEditIndex] = useState(-1);
-  const [error,setError]=useState("");
+  const [error, setError] = useState("");
+  //getting the todos using api
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        setList(response.data)
+      })
+      .catch((error) => setError(error))
+  }, [])
   const handleChange = (e) => {
     setTask(e.target.value);
   }
@@ -18,22 +26,34 @@ function App() {
     e.preventDefault();
     const trimmedTask = task.trim();
     if (trimmedTask.length === 0) return;
-    if (editIndex != -1) {
-      setList((prevList) => prevList.map((item, index) => index === editIndex ? { ...item, title: trimmedTask } : item));
-      setEditIndex(-1);
+    else if (editIndex != -1 && id <= 200) {
+      //If this action needs to be performed using API endpoint
+      console.log(list);
+      axios.put(`https://jsonplaceholder.typicode.com/todos/${list[editIndex].id}`, { ...list[editIndex], title: trimmedTask })
+        .then((response) => {
+          response.data;
+          setList((prevList) => prevList.map((item, index) => index === editIndex ? item = response.data : item));
+          setEditIndex(-1);
+          setError("")
+        })
+        .catch((error) => setError(error))
+    }
+    else if (editIndex != -1 && id > 200) {
+      setList((prevList) => prevList.map((item, index) => index === editIndex ? { ...item, title: trimmedTask } : item))
     }
     else {
-      axios.post("https://jsonplaceholder.typicode.com/todos", { title: trimmedTask, completed: false })
-      .then((response) => {
-        response.data;
-        console.log(response.data)
-        setList((prevList) => [...prevList, response.data]);
-      })
-      .catch((error)=>{
-        console.log("Error",error);
-        setError(error);
-      })
-      
+      const idno=list.length+1;
+      axios.post("https://jsonplaceholder.typicode.com/todos", { title: trimmedTask, completed: false, id:idno})
+        .then((response) => {
+          response.data;
+          const newItem={...response.data, id: idno}
+          console.log(newItem);
+          setList((prevList) => [newItem,...prevList]);
+        })
+        .catch((error) => {
+          console.log("Error", error);
+          setError(error);
+        })
     }
 
     setTask("");
@@ -41,12 +61,12 @@ function App() {
   const ToggleEvent = (toggleindex) => {
     setList((prevList) => prevList.map((item, index) => toggleindex === index ? { ...item, completed: !item.completed } : item))
   }
-  const handleEdit=(editIndex,title)=>{
+  const handleEdit = (editIndex, title) => {
     setEditIndex(editIndex);
     setTask(title);
   }
-  const handleDelete=(deleteindex)=>{
-    setList(prevList=>prevList.filter((listitem,index)=>index !== deleteindex))
+  const handleDelete = (deleteindex) => {
+    setList(prevList => prevList.filter((listitem, index) => index !== deleteindex))
   }
   return (
     <>
@@ -54,12 +74,12 @@ function App() {
         <p className="title-4xl font-bold title-blue-600">To Do List</p>
         <form  >
           <input type="title" placeholder="Add a new task..." value={task} className=" m-2 border title-xl font-semibold rounded-md py-4 px-2" onChange={handleChange}></input>
-          <button type="submit" className="border border-gray-200 bg-gray-600 hover-gray-200 title-xl font-semibold rounded-md py-4 px-2" onClick={handleSubmit}>{ editIndex!= -1 ? "Update task" : "Add Task"}</button>
+          <button type="submit" className="border border-gray-200 bg-gray-600 hover-gray-200 title-xl font-semibold rounded-md py-4 px-2" onClick={handleSubmit}>{editIndex != -1 ? "Update task" : "Add Task"}</button>
         </form>
         <ul>
-          {list.map((listitem, index) => <li key={index} className="flex gap-2"><span className={`cursor-pointer ${listitem.completed ? "line-through title-gray" : ""}"`} onClick={() => ToggleEvent(index)}>{listitem.title}</span>
-          <MdEdit className="cursor-pointer" onClick={() => {handleEdit(index,listitem.title)}
-          } /><RxCross2 className="cursor-pointer" onClick={()=>handleDelete(index)}/></li>)}
+          {list.map((listitem, index) => <li key={index} className="flex gap-2"><span className={`cursor-pointer ${listitem.completed ? "line-through " : ""}"`} onClick={() => ToggleEvent(index)}>{listitem.title}</span>
+            <MdEdit className="cursor-pointer" onClick={() => { handleEdit(index, listitem.title) }
+            } /><RxCross2 className="cursor-pointer" onClick={() => handleDelete(index)} /></li>)}
         </ul>
       </div>
     </>
